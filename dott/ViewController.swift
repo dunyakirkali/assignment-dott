817ee11c09db3cc90d7baa7b5bbdfb34874d4da3
Dunya Kirkali <dunyakirkali@gmail.com>
2019-12-16 19:24:06 +0100

    Create xcodeproj

---
+++ dott/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dott
+//
+//  Created by Dunya Kirkali on 16/12/2019.
+//  Copyright © 2019 Dunya Kirkali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
