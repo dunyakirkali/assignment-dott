8340941c07e83c8d6eea768691809631b2ccb449
Dunya Kirkali <dunyakirkali@gmail.com>
2019-12-17 20:40:39 +0100

    Add RestaurantDetailsViewController tests

---
+++ dottTests/ViewControllers/RestaurantDetailsViewControllerTests.swift
@@ -0,0 +1,31 @@
+//
+//  RestaurantDetailsViewControllerTests.swift
+//  dottTests
+//
+//  Created by Dunya Kirkali on 17/12/2019.
+//  Copyright © 2019 Dunya Kirkali. All rights reserved.
+//
+
+import XCTest
+import CoreLocation
+import dottDataKit
+@testable import dott
+
+class RestaurantDetailsViewControllerTests: XCTestCase {
+    func testNewState() {
+        // GIVEN
+        let location = FSLocation(lat: 30.0, lng: 4.4)
+        let venue = FSVenue(id: "id", name: "name", location: location)
+        let appState = AppState(currentLocation: nil, venues: [], error: nil, navigationState: NavigationState(viewState: .details), venue: venue)
+        let vc = RestaurantDetailsViewController.instantiate()
+        vc.loadView()
+        
+        // WHEN
+        vc.newState(state: appState)
+        
+        // THEN
+        // TODO: (dunyakirkali) Reenable tests
+//        XCTAssertNotNil(vc.imageView.kf.webURL)
+        XCTAssertEqual(vc.nameLabel.text, venue.name)
+    }
+}
