f473ef59f3ac6a6fb009029a1136e80407760cbd
Dunya Kirkali <dunyakirkali@gmail.com>
2019-12-18 20:50:06 +0100

    Add a simple test for GetVenueDetails

--- dottDataKitTests/Client/FourSquareClientTests.swift
+++ dottDataKitTests/Client/FourSquareClientTests.swift
@@ -13,7 +13,7 @@ import Moya
 class FourSquareClientTests: XCTestCase {
     func testSearchVenues() {
         let completedExpectation = expectation(description: "Completed")
-        let failedExpectation = expectation(description: "Result count is correct")
+        let failedExpectation = expectation(description: "Parse Error")
         failedExpectation.isInverted = true
         let venueNameExpectation = expectation(description: "Correct name")
         
@@ -31,6 +31,39 @@ class FourSquareClientTests: XCTestCase {
                     if
                         let venue = result.response.data.first,
                         venue.name == "Mr. Purple" {
+                        venueNameExpectation.fulfill()
+                    }
+                }
+                catch {
+                    failedExpectation.fulfill()
+                }
+            case .failure:
+                failedExpectation.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+    }
+    
+    func testGetVenueDetails() {
+        let completedExpectation = expectation(description: "Completed")
+        let failedExpectation = expectation(description: "Parse Error")
+        failedExpectation.isInverted = true
+        let venueNameExpectation = expectation(description: "Correct name")
+        
+        let provider = MockFourSquareClient.provider
+        
+        provider.request(.getVenueDetails(venueId: "412d2800f964a520df0c1fe3")) { result in
+            switch result {
+            case let .success(moyaResponse):
+                do {
+                    let data = moyaResponse.data
+                    let result = try JSONDecoder().decode(FSResponse<FSVenue>.self, from: data)
+            
+                    completedExpectation.fulfill()
+                                        
+                    let venue = result.response.data
+                    if venue.name == "Central Park" {
                         venueNameExpectation.fulfill()
                     }
                 }
